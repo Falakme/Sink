2cdee69177a11b68a3cba85934d4374a9e75539a
Aman Sanoj <amansanoj08@gmail.com>
2025-09-07 16:23:57 +0400

    Changes

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,14 +1,14 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  title: 'Falak Link',
+  email: '',
+  github: '',
+  twitter: '',
+  telegram: '',
+  mastodon: '',
+  blog: '',
+  description: 'A Secure Link Shortener with Analytics.',
   image: 'https://sink.cool/banner.png',
-  previewTTL: 300, // 5 minutes
+  previewTTL: 300,
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
     'dashboard',
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
 
   routeRules: {
     '/': {
-      prerender: true,
+      redirect: '/dashboard',
     },
     '/dashboard/**': {
       prerender: true,
